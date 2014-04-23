2581e040395a791492bba3a2d31e29332e7db047
Adrian Seyboldt <adrian.seyboldt@gmail.com>
2014-04-23 18:16:41 +0200

    cleanup in sort_xml_tags

--- sort_xml_tags.py
+++ sort_xml_tags.py
@@ -4,16 +4,25 @@ import xml.etree.ElementTree as ET
 import sys
 from io import BytesIO
 
-if __name__ == '__main__':
-    tree = ET.parse(sys.argv[1])
-    root = tree.getroot()
-    for elem in tree.iter():
+
+def sort_xml(xml_root):
+    for elem in xml_root.iter():
         children = elem.getchildren()
         attrib, text = elem.attrib, elem.text
         elem.clear()
         elem.attrib, elem.text = attrib, text
         elem.extend(sorted(children, key=lambda e: e.tag))
 
+
+def xml_to_str(xml_root):
     out = BytesIO()
+    tree = ET.ElementTree(xml_root)
     tree.write(out)
-    sys.stdout.write(out.getvalue().decode())
+    return out.getvalue().decode()
+
+
+if __name__ == '__main__':
+    tree = ET.parse(sys.argv[1])
+    root = tree.getroot()
+    sort_xml(root)
+    sys.stdout.write(xml_to_str(root))
