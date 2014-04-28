d7d43c6e02e8224baaa4a8fd70d32b96e9fcf01a
Adrian Seyboldt <adrian.seyboldt@gmail.com>
2014-04-28 18:24:41 +0200

    Use one thread for each request

--- mqdaemon.py
+++ mqdaemon.py
@@ -1,10 +1,39 @@
+#!/usr/bin/env python
+"""
+Start a daemon, that listens for new directories. Each new directory that
+satisfies some conditions will be interpreted as a request to run MaxQuant on
+the files in that directory. The protocoll that is used to communicate failure
+or success is described in `fscall.py`.
+
+Load balancing
+--------------
+This program uses a semaphore to limit the number of instances that run
+MaxQuant at the same time. If a task can not get access to MaxQuant after some
+amount of time, its status will be set to FAILED. If a task runs for too long
+it should be canceled, but this is not implemented yet (TODO).
+
+Security
+--------
+mqdaemon will not follow symlinks that point outside the task directory.  Tasks
+that use such symlinks as input files will fail.
+
+All output and log files will be readable by *any user*, so if a request should
+be private, their directory name should be impossible to guess (maybe use an
+uuid4?). The directory containing the tasks should not have its execute bit
+set for those users who are not allowed to access data. I am not really happy
+about this solution, but for our particular setup I can not really see a
+good alternative.
+
+"""
+
 from pathlib import Path
-import queue
 import threading
 import logging
+import argparse
+import sys
+import os
 try:
     import yaml
-
 except ImportError:
     yaml = None
 import json
@@ -12,20 +41,65 @@ import json
 import fscall
 import mqparams
 
-MAXQUEUE = 5
-LISTENDIR = Path().resolve()
-NUM_WORKERS = 5
-TASK_RE = "input\_[a-zA-Z0-9]*"
-BEAT_INTERVAL = 2
-MQBINPATH = Path('c:/Users/adr/Desktop/MaxQuant/bin/MaxQuantCmd.exe')
 
-logging.basicConfig(
-    level=logging.DEBUG,
-    filename='maxquant.log',
-)
+MQBINPATH = Path() / "todo"
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Run MaxQuant on input/parameter files under listendir"
+    )
+    parser.add_argument('listendir', help="directory where tasks are dumped",
+                        type=Path)
+    parser.add_argument('-n', '--num-workers',
+                        help="number of worker threads",
+                        type=int, default=2)
+    parser.add_argument('-s', '--timeout',
+                        help="maximal time to wait until computation starts",
+                        type=int, default=5)
+    parser.add_argument('-b', '--read-interval',
+                        help="seconds between scans for new tasks",
+                        type=int, default=10)
+    parser.add_argument('--mqpath', help="path tho MaxQuant binary",
+                        type=Path, default=MQBINPATH)
+    parser.add_argument('--task-re', help="regular expression for tasks",
+                        default=None)
+    parser.add_argument('--logfile',
+                        help="global logfile for all MaxQuant runs",
+                        default='./maxquant.log',
+                        type=Path)
+
+    args = parser.parse_args()
+    if not args.mqpath.is_file():
+        #print("No such file: {}".format(args.mqpath), file=sys.stderr)
+        sys.exit(1)
+
+    if not args.listendir.is_dir():
+        #print("Not a directory: {}".format(args.listendir), file=sys.stderr)
+        sys.exit(1)
+
+    return args
+
+
+def is_valid_file_below(log, file, basedir):
+    """ Test if file is below basedir and can be accessed. """
+    try:
+        file = file.resolve()
+    except OSError:
+        log.warn("Invalid input file " + str(file), exc_info=True)
+        return False
+    if not file.parent == basedir:
+        log.warn("Input file {} not in {}. Ignoring"
+                 .format(str(basedir), str(file)))
+        return False
+    if not os.access(str(file), os.R_OK):
+        log.warn("Can not read " + str(file))
+        return False
+    return True
 
 
 def get_files(log, infiles):
+    """ Seperate the infiles into raw, yaml/json and fasta files """
     raw_files = {}
     fasta_files = {}
     param_files = {}
@@ -52,6 +126,7 @@ def get_files(log, infiles):
 
 def parse_param_file(log, param_file):
     if param_file.suffix.lower() == '.yaml':
+        log.debug("Found yaml parameter file: " + str(param_file))
         if yaml is None:
             log.error("No YAML support")
             raise ValueError("No YAML support")
@@ -64,6 +139,7 @@ def parse_param_file(log, param_file):
             log.error("Could not read parameter file")
             raise
     elif param_file.suffix.lower() == '.json':
+        log.debug("Found json parameter file: " + str(param_file))
         try:
             params = json.loads(param_file.open().read())
         except ValueError:
@@ -78,88 +154,113 @@ def parse_param_file(log, param_file):
     return params
 
 
-def run_maxquant(log, infiles, outdir, tmpdir):
+def run_maxquant(log, infiles, outdir, tmpdir, workdir):
+    """ Run MaxQuant on infiles and return paths of resulting files.
+
+    Arguments
+    ---------
+    log : logging.Logger
+        Logger for all events while running MaxQuant
+
+    infiles : list of pathlib.Path
+        Input files, including parameter file (json/yaml) for MaxQuant
+
+    outdir : pathlib.Path
+        Directory where to put output files of MaxQuant
+
+    tmpdir : pathlib.Path
+        Temporary directory for MaxQuant
+
+    workdir : pathlib.Path
+        Work directory for MaxQuant. All input files of MaxQuant will
+        be copied here. This directory will be created by this function.
+
+    Returns
+    -------
+    List of pathlib.Path for each output file of MaxQuant
+    """
+
+    log.debug("read input files")
     param_file, raw_files, fasta_files = get_files(log, infiles)
+
+    log.debug("parse parameter file")
     params = parse_param_file(log, param_file)
 
     try:
         result = mqparams.mqrun(
             MQBINPATH, params, raw_files, fasta_files, outdir, tmpdir, log
         )
-    except Exception as e:
-        log.error("Could not execute MaxQuant: " + str(e))
+    except Exception:
+        log.exception("Could not execute MaxQuant.")
         raise
 
     return result
 
 
-def fill_queue(task_queue, listener):
+def serve(listener, num_workers, timeout):
+    """ Listen for new tasks in listendir and start worker thread. """
+    sem = threading.BoundedSemaphore(num_workers)
     for task in listener:
-        try:
-            task.status("WAITING")
-            task._start_beat(BEAT_INTERVAL)
+        task_thread = threading.Thread(
+            target=worker, name="worker-{}".format(task.uuid),
+            args=(task, sem, timeout)
+        )
+        task.log.info("Create thread for new task " + task.uuid)
+        task_thread.start()
+
+
+def worker(task, sem, timeout=None):
+    """ Run MaxQuant for given task. """
+    try:
+        with task.beat():
+            task.log.debug("Checking input files")
+            for file in task.infiles:
+                if not is_valid_file_below(task.log, file, task._dir):
+                    task.log.error("Invalid input file: %s, exiting", file)
+                    task.error("Invalid input file {}".format(file))
+                    return
+            task.log.debug("Input files ok")
+            task.log.info("Wait for resources")
+            task.status('WAITING')
+            if not sem.acquire(timeout=timeout):
+                msg = "Timeout. No resources available."
+                task.log.error(msg)
+                task.error(msg)
+                return
+
             try:
-                logging.info("Add new task to queue: " + task.uuid)
-                logging.info("Size of queue is approx "
-                             + str(task_queue.qsize()))
-                task_queue.put_nowait(task)
-            except queue.Full:
-                task._stop_beat()
-                logging.error("Queue is full. Can't add task " + task.uuid)
-                task.error("Compute node overloaded")
-        except Exception as e:
-            logging.critical("Unknown exception in listener thread: " + str(e))
+                task.status('WORKING')
+                outfiles = run_maxquant(
+                    task.log, task.infiles, task.outdir, task.outdir
+                )
+                task.success(outfiles)
+            except Exception:
+                task.log.exception("Error running MaxQuant")
+                task.error("MaxQuant failed.")
+            finally:
+                sem.release()
 
-
-def worker(task_queue):
-    while True:
-        try:
-            task = task_queue.get()
-            task.status('WORKING')
-            task._stop_beat()
-            with task.beat(BEAT_INTERVAL):
-                try:
-                    outfiles = run_maxquant(
-                        task.log, task.infiles, task.outdir, task.outdir
-                    )
-                except Exception as e:
-                    task.error(str(e))
-                else:
-                    task.outfiles = outfiles
-                    task.success()
-        except Exception as e:
-            logging.critical("Unknown error in worker thread: " + str(e))
+    except Exception:
+        logging.exception("Unknown error in worker thread")
 
 
 def main():
-    logging.info("Starting daemon")
-    logging.info("LISTENDIR is " + str(LISTENDIR))
-    logging.info("MAXQUEUE is " + str(MAXQUEUE))
-    logging.info("NUM_WORKERS is " + str(NUM_WORKERS))
-    logging.info("Path to MaxQuant is " + str(MQBINPATH))
+    args = parse_args()
+    logging.basicConfig(
+        level=logging.DEBUG,
+        filename=str(args.logfile),
+    )
+    logging.info("starting daemon")
+    logging.info("listendir is " + str(args.listendir))
+    logging.info("timeout is " + str(args.timeout))
+    logging.info("num_workers is " + str(args.num_workers))
+    logging.info("path to maxquant is " + str(args.mqpath))
     listener = fscall.listen(
-        LISTENDIR,
-        task_re=TASK_RE,
+        args.listendir,
+        task_re=args.task_re,
     )
-    task_queue = queue.Queue(maxsize=MAXQUEUE)
-    fill_thread = threading.Thread(
-        target=fill_queue,
-        name="receive_tread",
-        args=(task_queue, listener),
-    )
-    logging.info('Start to listen in directory ' + str(LISTENDIR))
-    fill_thread.start()
-
-    workers = []
-    for i in range(NUM_WORKERS):
-        thread = threading.Thread(
-            target=worker,
-            name="worker-{}".format(i),
-            args=(task_queue,),
-        )
-        workers.append(thread)
-        logging.info('Starting worker "{}"'.format(thread.name))
-        thread.start()
+    logging.info('start to listen in directory ' + str(args.listendir))
+    serve(listener, args.num_workers, args.timeout)
 
 
 if __name__ == '__main__':
